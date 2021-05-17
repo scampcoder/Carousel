9827e87a19389632ac0d19f9d5e42009ea07e77c
Victoria Kee <victoriakee95@gmail.com>
2021-05-17 14:10:02 -0500

    Set up btns

--- backend/app.js
+++ backend/app.js
@@ -2,3 +2,15 @@ const slides = document.querySelectorAll('.slide');
 const nextBtn = document.querySelector('.nextBtn');
 const prevBtn = document.querySelector('.prevBtn');
 
+slides.forEach(function(slide, index){
+    slide.style.left = `${index * 100}%`
+});
+
+let counter = 0;
+nextBtn.addEventListener('click', function(){
+    counter++;
+})
+
+prevBtn.addEventListener('click', function(){
+    counter--;
+})
