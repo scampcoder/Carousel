edd263dbef32417c754a53ad7907707ac166bd61
Victoria Kee <victoriakee95@gmail.com>
2021-05-17 14:27:43 -0500

    Hide option 2 for slider

--- backend/app.js
+++ backend/app.js
@@ -20,28 +20,28 @@ prevBtn.addEventListener('click', function(){
 
 function carousel(){
     //working with slides
-    // if(counter === slides.length){
-    //     counter = 0;
-    // }
-    // if(counter < 0){
-    //     counter = slides.length - 1;
-    // }
+    if(counter === slides.length){
+        counter = 0;
+    }
+    if(counter < 0){
+        counter = slides.length - 1;
+    }
 
     //working with btns
-    if(counter < slides.length - 1){
-        nextBtn.style.display = 'block';
-    }
-    else{
-        nextBtn.style.display = 'none';
-    }
-    if(counter > 0){
-        prevBtn.style.display = "block";
-    }
-    else{
-        prevBtn.style.display = "none";
-    }
+    // if(counter < slides.length - 1){
+    //     nextBtn.style.display = 'block';
+    // }
+    // else{
+    //     nextBtn.style.display = 'none';
+    // }
+    // if(counter > 0){
+    //     prevBtn.style.display = "block";
+    // }
+    // else{
+    //     prevBtn.style.display = "none";
+    // }
     slides.forEach(function(slide){
         slide.style.transform = `translateX(-${counter * 100}%)`
     })
 }
-prevBtn.style.display = "none"
+//prevBtn.style.display = "none"
